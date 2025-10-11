b2e80b84f45faff03adec02811c8ad1f9a671d2b
juanRCoder <guillecarck@gmail.com>
2025-10-11 17:13:46 -0500

    feat: create table for user credit requests

--- front/src/routes/route.tsx
+++ front/src/routes/route.tsx
@@ -6,6 +6,9 @@ import { Login } from '@/pages/Login'
 import { UserDashboard } from '@/pages/UserDashboard'
 import { RegisterPyme } from '@/pages/RegisterPyme'
 import { RegisterPymeDocuments } from '@/pages/RegisterPymeDocuments'
+import { UserPymesList } from '@/components/UserPymesList'
+import { UserProfile } from '@/components/UserProfile'
+import { UserCreditRequests } from '@/components/UserCreditRequests'
 
 export const mainRouter = createBrowserRouter([
   {
@@ -23,7 +26,21 @@ export const mainRouter = createBrowserRouter([
   },
   {
     path: '/Dashboard',
-    element: <UserDashboard />
+    element: <UserDashboard />,
+    children: [
+       {
+        index: true,
+        element: <UserProfile />,
+      },
+      {
+        path: 'Pymes',
+        element: <UserPymesList />,
+      },
+      {
+        path: 'Solicitudes',
+        element: <UserCreditRequests />,
+      }
+    ]
   },
   {
     path: '/Dashboard/RegistroPyme',
