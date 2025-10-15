21dc79d28a1fc59524ea64d4441b036c1177155a
Daniel <danny2003renato@gmail.com>
2025-10-15 12:41:17 -0500

    refactor: update AdminDashboard route to use wildcard for subroutes

--- front/src/routes/route.tsx
+++ front/src/routes/route.tsx
@@ -25,7 +25,7 @@ export const mainRouter = createBrowserRouter([
     element: <RegisterPyme />
   },
   {
-    path: '/AdminDashboard',
+    path: '/admin/*',
     element: <AdminDashboard />
   },
 
