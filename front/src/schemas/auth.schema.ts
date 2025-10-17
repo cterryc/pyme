b05381296518243f2857eff1ff0d8c5e7d647ce3
Jmjoss <josekandia42@gmail.com>
2025-10-17 14:28:14 -0300

    Remove the 8-character password message at login

--- front/src/schemas/auth.schema.ts
+++ front/src/schemas/auth.schema.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 export const loginSchema = z.object({
   email: z.string().email("Correo inválido"),
-  password: z.string().min(8, "La contraseña debe tener mínimo 8 caracteres"), 
+  password: z.string() 
 });
 
 export const registerSchema = loginSchema.extend({
