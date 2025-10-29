0933c1c3015c0df54933f6b6051de1f4e7b78428
Terry <danyel.martel@gmail.com>
2025-10-28 20:23:01 -0500

    fixed conflicts

--- backend/src/api/loan/routes.ts
+++ backend/src/api/loan/routes.ts
@@ -10,15 +10,14 @@ import {
   getCreditApplicationByIdParamsSchema,
   deleteCreditApplicationParamsSchema
 } from "./validator";
-import { validateUuid } from "../../middlewares/validateParamId.middleware";
 
 const loanRouter = Router();
 
-loanRouter.use(authenticate);
 
 // Protected routes
 loanRouter.post(
   "/",
+  authenticate,
   schemaValidator(loanRequestSchema, null),
   LoanController.loanRequest
 );
@@ -26,59 +25,48 @@ loanRouter.post(
 
 loanRouter.post(
     "/confirm",
+    authenticate,
     schemaValidator(createCreditApplicationSchema, null),
     LoanController.createCreditApplication
 );
 
 loanRouter.get(
     "/user",
+    authenticate,
     LoanController.listCreditApplicationsByUserId
 );
 
 loanRouter.get(
     "/status/:id",
-    validateUuid,
+    authenticate,
     schemaValidator(null, getCreditApplicationStatusParamsSchema),
     LoanController.getCreditApplicationStatus
 );
 
 loanRouter.get(
     "/",
+    authenticate,
     schemaValidator(null, listCreditApplicationsQuerySchema),
     LoanController.listCreditApplications
 );
 
 loanRouter.get(
     "/:id",
-<<<<<<< HEAD
-<<<<<<< HEAD
-    authenticate,
-    // schemaValidator(null, getCreditApplicationByIdParamsSchema),
-=======
-    validateUuid,
-    schemaValidator(null, getCreditApplicationByIdParamsSchema),
->>>>>>> 8d96d536b9fa85a0fa3dfe6b1aa9dcf283ca8809
-=======
 
     authenticate,
     // schemaValidator(null, getCreditApplicationByIdParamsSchema),
     // validateUuid,
     // schemaValidator(null, getCreditApplicationByIdParamsSchema),
->>>>>>> a4547e617995e3de8a61e7805fe7beb31bb76652
     LoanController.getCreditApplicationById
 );
 
 loanRouter.delete(
     "/:id",
-    validateUuid,
+    authenticate,
     schemaValidator(null, deleteCreditApplicationParamsSchema),
     LoanController.deleteCreditApplication
 );
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> a4547e617995e3de8a61e7805fe7beb31bb76652
 // loanRouter.post(
 //     "/firma",
 //     // schemaValidator(createCreditApplicationSchema, null),
@@ -90,18 +78,5 @@ loanRouter.delete(
 //     // schemaValidator(createCreditApplicationSchema, null),
 //     LoanController.apiFirma
 // );
-<<<<<<< HEAD
-=======
-loanRouter.post(
-    "/firma",
-    // schemaValidator(createCreditApplicationSchema, null),
-    (req:any, res:any) => {
-        console.log(req.body);
-        res.status(200).json({message: "Gracias"});
-    }
-);
->>>>>>> 8d96d536b9fa85a0fa3dfe6b1aa9dcf283ca8809
-=======
->>>>>>> a4547e617995e3de8a61e7805fe7beb31bb76652
 
 export default loanRouter;
