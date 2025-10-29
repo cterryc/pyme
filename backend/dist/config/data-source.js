a2474aaa55ba9e30b7588f25d842567f72a5660c
Nickock <=>
2025-10-28 21:19:57 -0300

    .

--- backend/dist/config/data-source.js
+++ backend/dist/config/data-source.js
@@ -15,7 +15,7 @@ exports.AppDataSource = new typeorm_1.DataSource({
     password: enviroment_config_1.default.DB_PASSWORD,
     database: enviroment_config_1.default.DB_NAME,
     ssl: enviroment_config_1.default.DB_SSL ? { rejectUnauthorized: false } : false,
-    synchronize: true, // Auto-sync in dev only
+    synchronize: false, // Auto-sync in dev only
     logging: false,
     entities: ["src/entities/**/*.js"],
     migrations: ["src/migrations/**/*.js"],
