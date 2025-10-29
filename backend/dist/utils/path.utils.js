e78156d3d6953763c5cc72b2d7ffeea3d7b95bad
Nickock <=>
2025-10-28 21:33:38 -0300

    nope

--- backend/dist/utils/path.utils.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.rootPath = void 0;
-// LIBRARIES
-const path_1 = require("path");
-const currentFilePath = __filename;
-const currentDir = (0, path_1.dirname)(currentFilePath);
-const rootPath = (0, path_1.resolve)(currentDir, "../");
-exports.rootPath = rootPath;
