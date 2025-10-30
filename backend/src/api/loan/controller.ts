773cd3e8e637791bbe3b8ed7634ce8c4919af325
Nickock <=>
2025-10-29 21:20:19 -0300

    cambios necesarios para que funcione el tema de firma y eso

--- backend/src/api/loan/controller.ts
+++ backend/src/api/loan/controller.ts
@@ -1,150 +1,107 @@
-import { Request, Response, NextFunction } from "express";
-import { HttpStatus } from "../../constants/HttpStatus";
-import apiResponse from "../../utils/apiResponse.utils";
-import LoanService from "./service";
-import { createCreditApplicationSchema, loanRequestSchema } from "./validator";
-import { responseLoanRequest } from "./interface";
+import { Request, Response, NextFunction } from 'express'
+import { HttpStatus } from '../../constants/HttpStatus'
+import apiResponse from '../../utils/apiResponse.utils'
+import LoanService from './service'
+import { createCreditApplicationSchema, loanRequestSchema } from './validator'
+import { responseLoanRequest } from './interface'
 
 export default class LoanController {
-  private static loanService = new LoanService();
+  private static loanService = new LoanService()
 
-  static loanRequest = async (
-    req: Request,
-    res: Response,
-    next: NextFunction
-  ) => {
+  static loanRequest = async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const userId = res.locals.user?.id as string;
-      const companyId = loanRequestSchema.parse(req.body).companyId;
+      const userId = res.locals.user?.id as string
+      const companyId = loanRequestSchema.parse(req.body).companyId
 
-      const formLoanRequest = await this.loanService.loanRequest(
-        userId,
-        companyId
-      );
+      const formLoanRequest = await this.loanService.loanRequest(userId, companyId)
 
-      res.status(HttpStatus.OK).json(apiResponse(true, formLoanRequest));
+      res.status(HttpStatus.OK).json(apiResponse(true, formLoanRequest))
     } catch (error) {
-      return next(error);
+      return next(error)
     }
-  };
+  }
 
-  static createCreditApplication = async (
-    req: Request,
-    res: Response,
-    next: NextFunction
-  ) => {
+  static createCreditApplication = async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const userId = res.locals.user?.id as string;
-      const loanData = createCreditApplicationSchema.parse(req.body);
+      const userId = res.locals.user?.id as string
+      const loanData = createCreditApplicationSchema.parse(req.body)
 
-      const loanRequest: responseLoanRequest | null =
-        await this.loanService.createCreditApplication(
-          loanData.id,
-          loanData.selectedAmount,
-          loanData.selectedTermMonths,
-          loanData.companyId,
-          userId
-        );
+      const loanRequest: responseLoanRequest | null = await this.loanService.createCreditApplication(
+        loanData.id,
+        loanData.selectedAmount,
+        loanData.selectedTermMonths,
+        loanData.companyId,
+        userId
+      )
 
-      res.status(HttpStatus.CREATED).json(apiResponse(true, loanRequest));
+      res.status(HttpStatus.CREATED).json(apiResponse(true, loanRequest))
     } catch (error) {
-      return next(error);
+      return next(error)
     }
-  };
+  }
 
-  static listCreditApplicationsByUserId = async (
-    req: Request,
-    res: Response,
-    next: NextFunction
-  ) => {
+  static listCreditApplicationsByUserId = async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const userId = res.locals.user?.id as string;
-      const applications =
-        await this.loanService.listCreditApplicationsByUserId(userId);
-      res.status(HttpStatus.OK).json(apiResponse(true, applications));
+      const userId = res.locals.user?.id as string
+      const applications = await this.loanService.listCreditApplicationsByUserId(userId)
+      res.status(HttpStatus.OK).json(apiResponse(true, applications))
     } catch (error) {
-      return next(error);
+      return next(error)
     }
-  };
+  }
 
-  static getCreditApplicationStatus = async (
-    req: Request,
-    res: Response,
-    next: NextFunction
-  ) => {
+  static getCreditApplicationStatus = async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const { id } = req.params;
-      const statusInfo = await this.loanService.getCreditApplicationStatus(id);
-      res.status(HttpStatus.OK).json(apiResponse(true, statusInfo));
+      const { id } = req.params
+      const statusInfo = await this.loanService.getCreditApplicationStatus(id)
+      res.status(HttpStatus.OK).json(apiResponse(true, statusInfo))
     } catch (error) {
-      return next(error);
+      return next(error)
     }
-  };
+  }
 
-  static listCreditApplications = async (
-    req: Request,
-    res: Response,
-    next: NextFunction
-  ) => {
+  static listCreditApplications = async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const { page, limit, status } = req.query;
+      const { page, limit, status } = req.query
       const result = await this.loanService.listCreditApplications(
         page as string | number,
         limit as string | number,
         status as string
-      );
-      res.status(HttpStatus.OK).json(apiResponse(true, result));
+      )
+      res.status(HttpStatus.OK).json(apiResponse(true, result))
     } catch (error) {
-      return next(error);
+      return next(error)
     }
-  };
+  }
 
-  static getCreditApplicationById = async (
-    req: Request,
-    res: Response,
-    next: NextFunction
-  ) => {
+  static getCreditApplicationById = async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const { id } = req.params;
-      const application = await this.loanService.getCreditApplicationById(id);
-      res.status(HttpStatus.OK).json(apiResponse(true, application));
+      const { id } = req.params
+      const application = await this.loanService.getCreditApplicationById(id)
+      res.status(HttpStatus.OK).json(apiResponse(true, application))
     } catch (error) {
-      return next(error);
+      return next(error)
     }
-  };
+  }
 
-  static deleteCreditApplication = async (
-    req: Request,
-    res: Response,
-    next: NextFunction
-  ) => {
+  static deleteCreditApplication = async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const { id } = req.params;
-      const userId = res.locals.user?.id as string;
-      const result = await this.loanService.deleteCreditApplication(id, userId);
-      res.status(HttpStatus.OK).json(apiResponse(true, result));
+      const { id } = req.params
+      const userId = res.locals.user?.id as string
+      const result = await this.loanService.deleteCreditApplication(id, userId)
+      res.status(HttpStatus.OK).json(apiResponse(true, result))
     } catch (error) {
-      return next(error);
+      return next(error)
     }
-  };
+  }
 
   // --- MÉTODOS ADMINISTRATIVOS ---
 
-  static updateCreditApplicationStatus = async (
-    req: Request,
-    res: Response,
-    next: NextFunction
-  ) => {
+  static updateCreditApplicationStatus = async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const applicationId = req.params.id;
-      const adminUserId = res.locals.user?.id as string;
-      const {
-        newStatus,
-        rejectionReason,
-        internalNotes,
-        approvedAmount,
-        riskScore,
-      } = req.body;
+      const applicationId = req.params.id
+      const adminUserId = res.locals.user?.id as string
+      const { newStatus, rejectionReason, internalNotes, approvedAmount, riskScore } = req.body
 
       const result = await this.loanService.updateCreditApplicationStatus(
         applicationId,
@@ -154,45 +111,36 @@ export default class LoanController {
         internalNotes,
         approvedAmount,
         riskScore
-      );
+      )
 
-      res.status(HttpStatus.OK).json(apiResponse(true, result));
-    } catch (error) {
-      return next(error);
-    }
-  };
-
-  static getCreditApplicationsForAdmin = async (
-    req: Request,
-    res: Response,
-    next: NextFunction
-  ) => {
-    try {
-      const page = parseInt(req.query.page as string) || 1;
-      const limit = parseInt(req.query.limit as string) || 10;
-      const status = req.query.status as string;
-      const companyName = req.query.companyName as string;
-
-      const result = await this.loanService.getCreditApplicationsForAdmin(
-        page,
-        limit,
-        status as any,
-        companyName
-      );
-
-      res.status(HttpStatus.OK).json(apiResponse(true, result));
-    } catch (error) {
-      return next(error);
-    }
-  };
-
-  static apiFirma = async (req: Request, res: Response, next: NextFunction) => {
-    const body = req.body;
-    try {
-      const saveData = await this.loanService.apiFirma(body)
-      return res.status(saveData)
+      res.status(HttpStatus.OK).json(apiResponse(true, result))
     } catch (error) {
       return next(error)
     }
-  };
+  }
+
+  static getCreditApplicationsForAdmin = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const page = parseInt(req.query.page as string) || 1
+      const limit = parseInt(req.query.limit as string) || 10
+      const status = req.query.status as string
+      const companyName = req.query.companyName as string
+
+      const result = await this.loanService.getCreditApplicationsForAdmin(page, limit, status as any, companyName)
+
+      res.status(HttpStatus.OK).json(apiResponse(true, result))
+    } catch (error) {
+      return next(error)
+    }
+  }
+
+  static apiFirma = async (req: Request, res: Response, next: NextFunction) => {
+    const body = req.body
+    try {
+      const saveData = await this.loanService.apiFirma(body)
+      return res.status(saveData).json({ message: 'Gracias por firmarlo pa!' })
+    } catch (error) {
+      return next(error)
+    }
+  }
 }
