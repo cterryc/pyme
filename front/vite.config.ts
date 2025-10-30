3a850018889e718a6f7f599cf93e1c27a81220cc
Nickock <=>
2025-10-30 00:48:17 -0300

    add vercel config

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -5,9 +5,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-   resolve: {
+  resolve: {
     alias: {
-      '@': '/src',
-    },
+      '@': '/src'
+    }
   },
+  base: '/'
 })
