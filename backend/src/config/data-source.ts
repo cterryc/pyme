124b8b5d798811bc19367883b322a2f905218e79
Nickock <=>
2025-10-29 21:04:43 -0300

    fix para solventar tema firmas

--- backend/src/config/data-source.ts
+++ backend/src/config/data-source.ts
@@ -1,21 +1,21 @@
-import "reflect-metadata";
-import { DataSource } from "typeorm";
-import config from "./enviroment.config";
+import 'reflect-metadata'
+import { DataSource } from 'typeorm'
+import config from './enviroment.config'
 
-const isProd = process.env.NODE_ENV === "production";
+const isProd = process.env.NODE_ENV === 'production'
 
 export const AppDataSource = new DataSource({
-  type: "postgres",
+  type: 'postgres',
   host: config.DB_HOST,
   port: config.DB_PORT,
   username: config.DB_USERNAME,
   password: config.DB_PASSWORD,
   database: config.DB_NAME,
   ssl: config.DB_SSL ? { rejectUnauthorized: false } : false,
-  synchronize: !isProd, // Auto-sync in dev only, disabled in production
+  synchronize: true, // Auto-sync in dev only, disabled in production
   logging: false,
-  entities: [isProd ? "dist/entities/**/*.js" : "src/entities/**/*.ts"],
-  migrations: [isProd ? "dist/migrations/**/*.js" : "src/migrations/**/*.ts"],
-  subscribers: [isProd ? "dist/subscribers/**/*.js" : "src/subscribers/**/*.ts"],
+  entities: [isProd ? 'dist/entities/**/*.js' : 'src/entities/**/*.ts'],
+  migrations: [isProd ? 'dist/migrations/**/*.js' : 'src/migrations/**/*.ts'],
+  subscribers: [isProd ? 'dist/subscribers/**/*.js' : 'src/subscribers/**/*.ts']
   //   dropSchema: true,
-});
+})
